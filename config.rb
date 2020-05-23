1d4335a5266d81ffd17e905c9944d1c36123eab5
Alex Chaplinsky <alchaplinsky@gmail.com>
2020-05-23 13:20:06 +0200

    Version bump

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 GH_USERNAME = 'swiftyapp'
 GH_REPO = 'swifty'
-GH_TAG = '0.4.9'
+GH_TAG = '0.5.0'
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
